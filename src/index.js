f23cf3d23e4b27085170508308cd88765b9a7f7f
Cuongvd <vuduccuong.ck48@gmail.com>
2020-02-27 10:12:06 +0700

    ok

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>OK OK OK</h1>;
+};
+
+export default App;
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById("root")
+);
